15dcf670f73226ba9ef61a143cb68fced32e6145
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-27 12:21:21 +0530

    modified widget test dart file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:bawatrust_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BawaTrustApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

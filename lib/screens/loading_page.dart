9d4a70d4209f2cb67cf776ab19490c4e84abaa02
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-27 12:18:47 +0530

    added new loading page dart file

---
+++ lib/screens/loading_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart'; // Make sure this import is correct
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({super.key});
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Delay 3 seconds then navigate to LoginPage
+    Future.delayed(const Duration(seconds: 7), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFAC9A58),
+      body: Stack(
+        children: [
+          // Logo
+          Positioned(
+            top: screenHeight * 0.41,
+            left: screenWidth * 0.283,
+            child: Image.asset(
+              'assets/images/gbt_logo_white.png',
+              width: screenWidth * 0.424,
+              height: screenHeight * 0.118,
+              fit: BoxFit.contain,
+            ),
+          ),
+          // Bottom Text
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 340),
+              child: Text(
+                'GEOFFREY BAWA TRUST',
+                style: const TextStyle(
+                  fontFamily: 'CormorantGaramond-Bold',
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  height: 29 / 24,
+                  letterSpacing: 0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

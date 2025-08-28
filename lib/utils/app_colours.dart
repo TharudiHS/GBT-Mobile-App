4a7c5c981cb364d673603e36988853072af83487
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-28 13:44:56 +0530

    added new app_colours.dart file

---
+++ lib/utils/app_colours.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColours {
+  static const Color gold = Color(0xFFAC9A58);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color greyE3 = Color(0xFFE3E3E3);
+  static const Color greyB5 = Color(0xFFB5B5B5);
+  static const Color greyE5 = Color(0xFFE5E5E5);
+  static const Color gold40 = Color(0xA6AC9A58);
+  static const Color goldOpacity40 = Color.fromRGBO(172, 154, 88, 0.4);
+  static const Color goldStrong = Color(0xFFAC9A58);
+  static const Color cream = Color(0xFFFFF9EC);
+  static const Color yellowAccent = Color.fromARGB(255, 245, 193, 5);
+}

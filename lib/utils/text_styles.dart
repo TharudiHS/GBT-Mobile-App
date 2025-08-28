be0fd81b6ac516a99ab1b728e9b79b3de12e7ac7
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-28 14:35:20 +0530

    added new file as text_styles.dart

---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'app_colours.dart';
+
+class TextStyles {
+  static const TextStyle size24WeightBoldCormorantGaramondBold = TextStyle(
+    fontFamily: 'CormorantGaramond-Bold',
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColours.white,
+    height: 29 / 24,
+    letterSpacing: 0,
+  );
+}

6f7f4150b575c37d644a061c5eb50e73d84315c0
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-28 14:45:06 +0530

    added new styles

--- lib/utils/text_styles.dart
+++ lib/utils/text_styles.dart
@@ -10,4 +10,27 @@ class TextStyles {
     height: 29 / 24,
     letterSpacing: 0,
   );
+  static const TextStyle size24WeightBoldCormorantGaramondBoldblack = TextStyle(
+    fontFamily: 'CormorantGaramond-Bold',
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColours.black,
+  );
+  static const TextStyle size12TTCommonsItalic = TextStyle(
+    fontFamily: 'TT Commons Italic',
+    color: AppColours.greyB5,
+    fontSize: 12,
+  );
+  static const TextStyle size14WeightBoldTTCommonsRegular = TextStyle(
+    fontFamily: 'TT Commons Regular',
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    color: AppColours.black,
+  );
+  static const TextStyle size17WeightBoldTTCommonsRegular = TextStyle(
+    fontFamily: 'TT Commons Regular',
+    color: AppColours.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 17,
+  );
 }
